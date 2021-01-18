4d8fad9cb07a0dc275c4c301db4a04d526b1273b
davidjayjackson <davidjayjackson@gmail.como>
2021-01-18 15:22:10 -0500

    Updated 2021-01-18

--- API.R
+++ API.R
@@ -1,6 +1,23 @@
-# library(dplyr)
-# library(httr)
-# library(jsonlite)
+library(dplyr)
+library(ggplot2)
+library(scales)
 rm(list=ls())
-df <- data.table::fread("https://data.cdc.gov/resource/vsak-wrfu.csv")
+
 db <- read.csv("https://data.cdc.gov/resource/vsak-wrfu.csv")
+str(db)
+df <- db %>% select(state,week_ending_date,sex,age_group,total_deaths,covid_19_deaths) 
+colnames(df) <- c("State","Date","Sex","Age","Total","Covid19")
+## Fix Date
+df$Date <- substr(df$Date,1,10)
+df$Date <- as.Date(df$Date)
+## First Report
+all_all <- df %>% filter(Sex =="All Sex" & Age =="All Ages")
+all_all$Rate <- all_all$Covid19/all_all$Total
+ggplot(all_all) + geom_line(aes(x=Date,y=Rate)) +
+  scale_y_continuous(labels = percent) +
+  labs(title="Covid19 Deaths as a Perent of Total Deaths",
+       subtitle = "Total = All Diseases")
+
+### Import complete CDC Data 
+dat <- read.csv("./DATA/Weekly_by_Sex__Age.csv")
+ggplot(dat) + geom_line(aes(x=Date,y=Percent))
